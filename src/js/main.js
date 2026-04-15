737ad86eda95423cb24be4d293b1ec783797217f
Lareta064 <lareta1@yandex.ru>
2026-04-15 22:45:06 +0400

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
+  	/*MENU MOBILE */
+    const  bodyEl = document.body;
+	  const menuButton = document.querySelector('#menu-toggle');
+	  const menuNav = document.querySelector('#menu-wrapper');
+   
+     function closeMobileMenu(){
+        menuButton.classList.remove('active');
+        bodyEl.classList.remove('lock');
+        menuNav.classList.remove('active');
+      }
+      function openMobileMenu(){
+        menuButton.classList.add('active');
+        bodyEl.classList.add('lock');
+        menuNav.classList.add('active');
+      }
+     menuButton.addEventListener('click', function(){
+      if( menuButton.classList.contains('active')){
+        closeMobileMenu();
+      }else{
+         openMobileMenu();
+      }
+     });
+      
   const button = document.querySelector('.form-open');
   const anchor = document.querySelector('.form-open-anchor');
 
@@ -13,13 +36,29 @@ document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('scroll', toggleFloatButton, { passive: true });
   window.addEventListener('resize', toggleFloatButton);
   toggleFloatButton();
-
-  const backTop = document.querySelector('#backTop');
-  window.addEventListener('scroll', function(){
-	if(window.scrollY > 200){
-		backTop.classList.add('active');
-	}else{
-		backTop.classList.remove('active');
+  //popup
+	const popupBtns = document.querySelectorAll('[data-popup-btn]');
+	
+	if(popupBtns.length >0){
+		const  popups = document.querySelectorAll('[data-popup]');
+		
+		
+		popupBtns.forEach(btn=>{
+			btn.addEventListener('click', ()=>{
+				const  popupIsOpen = document.querySelector('.popup-open');
+				if(popupIsOpen ){
+					popupIsOpen.classList.remove('popup-open');
+					console.log(555);				
+				}
+				const btnData = btn.getAttribute('data-popup-btn');
+				console.log(btnData);
+				popups.forEach(pop=>{
+					const popupData = pop.dataset.popup;
+					if(btnData == popupData){
+						pop.classList.add('popup-open');	
+					}
+				})
+			})
+		})
 	}
-  })
 });
